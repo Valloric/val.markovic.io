9acdf161f348666db78bc3ffd6cc64aeb7b7b27f
Strahinja Val Markovic <val@markovic.io>
2013-07-28 18:23:05 -0700

    Adding s3cmd repo to vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,7 +21,21 @@ pip install httpie
 apt-get install -y npm
 
 # The easiest way to get node binaries on the PATH
-ln -fs /vagrant/node_modules/.bin /home/vagrant/bin
+mkdir /home/vagrant/bin
+
+for f in /vagrant/node_modules/.bin/*; do
+  ln -fs $f /home/vagrant/bin/$(basename $f)
+done
+
+if [[ ! -a /home/vagrant/s3cmd ]]
+then
+  git clone https://github.com/s3tools/s3cmd
+  cd s3cmd
+  git checkout 9ee3bdd320c82150a8d5c387dacc4d7f194cbecb
+  cd ..
+  sudo ln -sf /home/vagrant/s3cmd/s3cmd /home/vagrant/bin/s3cmd
+fi
+
 
 rm -rf /var/www
 ln -fs /vagrant/prod_deploy /var/www
